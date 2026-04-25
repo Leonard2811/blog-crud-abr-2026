f8d59cac0975875c37808d8f54330a8c5daa8e2e
Leonard2811 <leonardooliverosv2@.com>
2026-04-25 11:37:15 -0400

    article detail done

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,10 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Article
 
 # Create your views here.
-
 class ArticleListView(ListView):
     model = Article
     template_name = 'articles-list.html'
     context_object_name = 'articles_list'
-    
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article-detail.html'
+    context_object_name = 'article'
