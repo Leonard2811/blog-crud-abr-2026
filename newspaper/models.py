60589a89b98672792dacfb6e478d87f9f394b1f2
Leonard2811 <leonardooliverosv2@.com>
2026-04-25 10:53:41 -0400

    raticle list done

--- newspaper/models.py
+++ newspaper/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Article
 
 # Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'articles-list.html'
+    context_object_name = 'articles_list'
+    
